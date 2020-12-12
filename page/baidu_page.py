11bbd85d056a3c4410b330649dd176952dd04dff
fiddersik <49912129+FidderSik@users.noreply.github.com>
2020-12-12 07:42:24 +0000

    Update baidu_page.py

--- page/baidu_page.py
+++ page/baidu_page.py
@@ -4,6 +4,8 @@ from poium import Page, NewPageElement
 class BaiduPage(Page):
     search_input = NewPageElement(id_="kw", describe="搜索框")
     search_button = NewPageElement(id_="su", describe="搜索按钮")
-    settings = NewPageElement(link_text="设置", describe="设置下拉框")
+    ##可能是编码问题，我浏览器里的“设置”和文本框中的“设置”长得不一样，所以简单修改一下
+    ##settings = NewPageElement(link_text="设置", describe="设置下拉框")
+    settings = NewPageElement(id="s-usersetting-top", describe="设置下拉框")
     search_setting = NewPageElement(css=".setpref", describe="搜索设置选项")
     save_setting = NewPageElement(css=".prefpanelgo", describe="保存设置")
